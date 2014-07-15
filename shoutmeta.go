b9e734403195d5f4827fd488af7be538081b4714
David Leimbach <dave@Re64.local>
2014-07-14 20:20:31 -0700

    Now it's also an IRC bot

--- shoutmeta.go
+++ shoutmeta.go
@@ -7,6 +7,8 @@ import (
 	"fmt"
 	"bufio"
 	"os"
+	"net"
+	"strings"
 )
 
 type icyparseerror struct {
@@ -22,7 +24,7 @@ func parseIcy(rdr *bufio.Reader, c byte) (string, error) {
 	bytes := make([] byte, numbytes)
 	n, err :=  io.ReadFull(rdr, bytes)
 	if err != nil {
-		log.Fatal(err)
+		log.Panic(err)
 	}
 	if n != numbytes {
 		return "", &icyparseerror{"didn't get enough data"}  // may be invalid
@@ -39,16 +41,16 @@ func extractMetadata(rdr io.Reader, skip int) (<- chan string) {
 
 			_, err := io.ReadFull(bufrdr, skipbytes)
 			if err != nil {
-				log.Fatal(err)
+				log.Panic(err)
 			}
 			c, err := bufrdr.ReadByte()
 			if err != nil {
-				log.Fatal(err)
+				log.Panic(err)
 			}
 			if c > 0 {
 				meta, err := parseIcy(bufrdr, c)
 				if err != nil {
-					log.Fatal(err)
+					log.Panic(err)
 				}
 				ch <- meta
 			} 				
@@ -57,32 +59,102 @@ func extractMetadata(rdr io.Reader, skip int) (<- chan string) {
 	return ch
 }
 
-func main () {
+func shoutStreamStuff(url string) (<- chan string) {
 	log.Printf("Shoutcast stream metadata yanker v0.1\n");
 	client := &http.Client{}
-	
-	req, err := http.NewRequest("GET", os.Args[1], nil)
+
+	log.Printf("Getting from : %s\n", url)
+	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-		log.Fatal(err)
+		log.Panic(err)
 	}
 
 	req.Header.Add("Icy-MetaData", "1")
 	resp, err := client.Do(req)
 	if err != nil {
-		log.Fatal(err)
+		log.Panic(err)
 	}
 
 	amount := 0
 	_, err = fmt.Sscan(resp.Header.Get("Icy-Metaint"), &amount)
 	if err != nil {
-		log.Fatal(err)
+		log.Panic(err)
 	}
 
-	c := extractMetadata(resp.Body, amount)
-	for meta := range(c) {
-		fmt.Println(string(meta))
-	}
+	return extractMetadata(resp.Body, amount)
+}	
 
-	log.Fatal("Stream broke... dangit!")
+
+func ircStreamStuff(conn net.Conn, botname, channel string, mdc <- chan string) {
+	bior := bufio.NewReader(conn)
+	biow := bufio.NewWriter(conn)
+
+	fmt.Fprintf(biow, "NICK  %s\r\n", botname)
+	fmt.Fprintf(biow, "USER %s 0 * :tutorial bot\r\n", botname)
+	fmt.Fprintf(biow, "JOIN %s\r\n", channel)
+
+	cc := make(chan string)
+	go func () {
+		for { 
+			bytes, morep, err := bior.ReadLine()
+			if err != nil {
+				log.Panic(err)
+			}
+			for morep {
+				bytes2, morep, err := bior.ReadLine()
+				if err != nil {
+					log.Panic(err)
+				}
+				bytes = append(bytes, bytes2...)
+				if !morep {
+					break
+				}
+			}
+			cc <- string(bytes)
+		}
+	} ()
+			
+	for {
+		if err := biow.Flush(); err != nil {
+			log.Panic(err)
+		}
+		select {
+		case input:= <- mdc:
+			fmt.Fprintf(biow, "PRIVMSG %s : Now Playing: %s\r\n", channel, input)
+		case line := <- cc:
+			log.Print(line)
+				
+			// Handle server ping, so we don't get booted.
+			if strings.HasPrefix(line, "PING :") {
+				resp := strings.Replace(line, "PING", "PONG", 1)
+				fmt.Fprintf(biow, "%s\r\n", resp)
+			}
+		}
+	}
 }
-	
+
+func bot(serveraddy string, botname, channel string, metadata <- chan string) {
+	log.Print("Connecting to ", serveraddy)
+	conn, err := net.Dial("tcp4", serveraddy)
+	if err != nil {
+		log.Panic(err)
+	}
+	log.Print("Done")
+
+	ircStreamStuff(conn, botname, channel, metadata)
+}
+
+func usage () {
+	fmt.Printf("./shoutmeta irc.radioxenu.com:8000/relay irc.radioxenu.com:6667 metabot #radioxenu\n")
+	os.Exit(0)
+}
+
+func main () {
+	if len(os.Args) < 5 {
+		log.Print(len(os.Args))
+		usage()
+	}
+
+	bot(os.Args[2], os.Args[3], os.Args[4], shoutStreamStuff(os.Args[1]))
+	log.Panic("Stream broke... dangit!")
+}
